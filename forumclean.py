3c0698fc84cd30bdacae8524854772a6f3c8b805
Matt Salinero <matt.salinero@gmail.com>
2021-01-06 22:07:30 -0800

    feat: fleshed out data cleaning functions

--- forumclean.py
+++ forumclean.py
@@ -11,16 +11,23 @@ def clean_board_data(unclean_data, filepath=None):
         unclean_data = pd.DataFrame(unclean_data)
 
     # parse for topic number
-    clean_data['topic_id'] = unclean_data['topiclink'].apply(lambda x: int(x.split('=')[-1].split('.')[0]))
+    clean_data['topic_id'] = unclean_data['topiclink'].apply(lambda x: x.split('=')[-1].split('.')[0])
 
     # parse for creator info
     clean_data['creator'] = unclean_data['creator']
-    clean_data['creator_id'] = unclean_data['creatorlink'].apply(lambda x: int(x.split('=')[-1]))
+    clean_data['creator_id'] = unclean_data['creatorlink'].apply(lambda x: x.split('=')[-1])
 
     # parse for stats
     clean_data['views'] = unclean_data['views'].apply(lambda x: int(x.split()[0]))
     clean_data['replies'] = unclean_data['replies'].apply(lambda x: int(x.split()[0]))
 
+    # parse for board number
+    clean_data['board'] = unclean_data['url'].apply(lambda x: x.split("board=")[-1].split('.')[0])
+
+    clean_data['access_date'] = unclean_data['accessed']
+    clean_data['title'] = unclean_data['title']
+
     print(clean_data.head())
+    print(clean_data.info())
 
     return clean_data
