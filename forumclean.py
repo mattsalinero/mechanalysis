9a3e3385fd1284ed9b26201601fef11b4e3933ae
Matt Salinero <matt.salinero@gmail.com>
2021-01-07 23:05:09 -0800

    feat: added basic title parsing

--- forumclean.py
+++ forumclean.py
@@ -1,5 +1,6 @@
 import datetime
 import csv
+import regex
 import pandas as pd
 
 
@@ -31,3 +32,56 @@ def clean_board_data(unclean_data, filepath=None):
     print(clean_data.info())
 
     return clean_data
+
+
+def parse_title(to_search):
+
+    # find bracketed type indicator
+    gbreg = regex.compile(r"[[{(](\w*)[]})]")
+    gbtype = gbreg.match(to_search)
+    if gbtype:
+        gbreturn = gbtype.group(1)
+
+    else:
+        gbreturn = 'unknown'
+
+    type_set = ['GMK', 'PBT', 'ePBT', 'SA', 'HSA', 'KAT', 'KAM', 'DSA', 'IFK', 'JTK', 'CRP', 'SP', 'MDA', 'XDA', 'MG',
+                'INFINIKEY', 'MELGEEK', 'ENJOYPBT']
+
+    sep_set = [r"[:;,.]\s", r"\s[|-~/\\]", r"\sround", r"\sr\d"]
+
+    typereg = regex.compile(r"\b\L<type_set>\b", regex.IGNORECASE, type_set=type_set)
+    captype = typereg.search(to_search)
+    if captype:
+        typereturn = captype.group()
+    else:
+        typereturn = None
+    # types = typereg.finditer(to_search)
+    # typereturn = []
+    # for captype in types:
+    #     typereturn.append(captype.group())
+
+    if typereturn:
+        sepreg = regex.compile(r"\L<sep_set>",regex.IGNORECASE, sep_set=sep_set)
+        sep = sepreg.search(to_search, pos=captype.end())
+        titlelimit = [0, len(to_search)]
+        if gbtype:
+            titlelimit[0] = gbtype.end()
+        if sep:
+            titlelimit[1] = sep.start()
+
+        title = (to_search[titlelimit[0]:captype.start()] + to_search[captype.end():titlelimit[1]]).strip()
+
+        if not sep:
+            title = title.split()[0]
+
+        # define separator regex
+        # find separator (if exists)
+        # if it exists, grab stripped string between separator and type as title
+        # else just grab one word past the type
+        # if we can't find anything, set to 'unknown'
+        titlereturn = title
+    else:
+        titlereturn = None
+
+    return gbreturn, typereturn, titlereturn
--- initialtest.py
+++ initialtest.py
@@ -1,8 +1,11 @@
 from ghscrape import *
 from forumclean import *
 
-scraped_data = scrape_board("https://geekhack.org/index.php?board=70.", limit_pages=3,
-                            request_interval=1, filepath='test.csv')
-print(type(scraped_data))
+# scraped_data = scrape_board("https://geekhack.org/index.php?board=70.", limit_pages=3,
+#                             request_interval=1, filepath='test.csv')
+# print(type(scraped_data))
+#
+# clean_board_data(scraped_data)
 
-clean_board_data(scraped_data)
+print(find_captype("[GB] TGR-Jane v2 CE (QC, Shipping) 50% completed "))
+print(find_captype("[GB]Enjoypbt '紺桃KON MOMO' Japanese keycaps (7.10~7.30)"))
