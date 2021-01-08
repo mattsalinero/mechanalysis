9c78560942847e7d1744d1de17dff355de6c5291
Matt Salinero <matt.salinero@gmail.com>
2021-01-08 00:05:02 -0800

    feat: extend title parsing to find keyset names

--- forumclean.py
+++ forumclean.py
@@ -14,6 +14,9 @@ def clean_board_data(unclean_data, filepath=None):
     # parse for topic number
     clean_data['topic_id'] = unclean_data['topiclink'].apply(lambda x: x.split('=')[-1].split('.')[0])
 
+    # parse information from title
+    clean_data['thread_type'], clean_data['code'], clean_data['name'] = parse_titles(unclean_data['title'])
+
     # parse for creator info
     clean_data['creator'] = unclean_data['creator']
     clean_data['creator_id'] = unclean_data['creatorlink'].apply(lambda x: x.split('=')[-1])
@@ -28,13 +31,14 @@ def clean_board_data(unclean_data, filepath=None):
     clean_data['access_date'] = unclean_data['accessed']
     clean_data['title'] = unclean_data['title']
 
-    print(clean_data.head())
-    print(clean_data.info())
+    # print(clean_data.head())
+    # print(clean_data.info())
 
     return clean_data
 
-
 def parse_title(to_search):
+
+    to_search = to_search.strip('" ')
 
     # find bracketed type indicator
     gbreg = regex.compile(r"[[{(](\w*)[]})]")
@@ -48,8 +52,9 @@ def parse_title(to_search):
     type_set = ['GMK', 'PBT', 'ePBT', 'SA', 'HSA', 'KAT', 'KAM', 'DSA', 'IFK', 'JTK', 'CRP', 'SP', 'MDA', 'XDA', 'MG',
                 'INFINIKEY', 'MELGEEK', 'ENJOYPBT']
 
-    sep_set = [r"[:;,.]\s", r"\s[|-~/\\]", r"\sround", r"\sr\d"]
+    sep_set = [r"[:;,.]\s", r"\s[-|~/\\[{(]", r"\sround", r"\sr\d"]
 
+    # find main/first info code (for keycap)
     typereg = regex.compile(r"\b\L<type_set>\b", regex.IGNORECASE, type_set=type_set)
     captype = typereg.search(to_search)
     if captype:
@@ -61,27 +66,42 @@ def parse_title(to_search):
     # for captype in types:
     #     typereturn.append(captype.group())
 
+    # if we found a code, find likely set name in title
     if typereturn:
-        sepreg = regex.compile(r"\L<sep_set>",regex.IGNORECASE, sep_set=sep_set)
-        sep = sepreg.search(to_search, pos=captype.end())
+        # sepreg = regex.compile(r"\L<sep_set>", regex.IGNORECASE, sep_set=sep_set)
+        sepreg = regex.compile(r"\s[-|~/\\[{(]|[:;,.]\s", regex.IGNORECASE)
+        sep = sepreg.search(to_search, pos=5, endpos=len(to_search))
         titlelimit = [0, len(to_search)]
         if gbtype:
             titlelimit[0] = gbtype.end()
         if sep:
             titlelimit[1] = sep.start()
 
-        title = (to_search[titlelimit[0]:captype.start()] + to_search[captype.end():titlelimit[1]]).strip()
+        setname = (to_search[titlelimit[0]:captype.start()] + to_search[captype.end():titlelimit[1]]).strip()
 
-        if not sep:
-            title = title.split()[0]
+        if sep is None:
+            setname = setname.split()[0]
 
         # define separator regex
         # find separator (if exists)
         # if it exists, grab stripped string between separator and type as title
         # else just grab one word past the type
         # if we can't find anything, set to 'unknown'
-        titlereturn = title
+        namereturn = setname
     else:
-        titlereturn = None
+        namereturn = None
 
-    return gbreturn, typereturn, titlereturn
+    return gbreturn, typereturn, namereturn
+
+def parse_titles(titles):
+    threadtypes = []
+    capcodes = []
+    setnames = []
+
+    for title in titles:
+        parsed = parse_title(title)
+        threadtypes.append(parsed[0])
+        capcodes.append(parsed[1])
+        setnames.append(parsed[2])
+
+    return threadtypes, capcodes, setnames
--- initialtest.py
+++ initialtest.py
@@ -1,11 +1,13 @@
 from ghscrape import *
 from forumclean import *
 
-# scraped_data = scrape_board("https://geekhack.org/index.php?board=70.", limit_pages=3,
-#                             request_interval=1, filepath='test.csv')
+scraped_data = scrape_board("https://geekhack.org/index.php?board=70.", limit_pages=3,
+                            request_interval=1, filepath='test.csv')
 # print(type(scraped_data))
-#
-# clean_board_data(scraped_data)
 
-print(find_captype("[GB] TGR-Jane v2 CE (QC, Shipping) 50% completed "))
-print(find_captype("[GB]Enjoypbt '紺桃KON MOMO' Japanese keycaps (7.10~7.30)"))
+test = clean_board_data(scraped_data)[['code', 'name']]
+
+print(test.head(45))
+
+# print(find_captype("[GB] TGR-Jane v2 CE (QC, Shipping) 50% completed "))
+# print(find_captype("[GB]Enjoypbt '紺桃KON MOMO' Japanese keycaps (7.10~7.30)"))
