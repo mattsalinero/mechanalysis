422095f58ce3d7f8250f93cd8583ec153ffba4a6
Matt Salinero <matt.salinero@gmail.com>
2021-01-13 00:20:58 -0800

    refactor: begin implementing parser for keyset name extraction (not yet functional)

--- forumclean.py
+++ forumclean.py
@@ -2,6 +2,7 @@ import datetime
 import csv
 import regex
 import pandas as pd
+from lark import Lark
 
 
 def clean_board_data(unclean_data, filepath=None):
@@ -11,6 +12,7 @@ def clean_board_data(unclean_data, filepath=None):
     :param filepath:
     :return: DataFrame of cleaned data
     """
+    # TODO: potentially move clean_board_data() to the initial scraping step, break parse_title into own thing
     clean_data = pd.DataFrame()
     # print(str(type(unclean_data)) + str(type(unclean_data[0])))
     if type(unclean_data) is list and type(unclean_data[0]) is dict:
@@ -50,60 +52,105 @@ def parse_title(to_search):
     :return: tuple(str thread type, str infocode, str set name)
     """
     # TODO: improve implementation to use actual parser with more sophisticated grammar
-    to_search = to_search.strip('" ')
 
-    # find bracketed type indicator
-    gbreg = regex.compile(r"[[{(](\w*)[]})]")
-    gbtype = gbreg.match(to_search)
-    if gbtype:
-        gbreturn = gbtype.group(1)
+    title_grammar = r'''
+        topic: (threadcode? titlesection endsection?) | (threadcode? invtitlesection endsection?)
+        threadcode: _LEADBLOCK* _BRACO /\w+/ _BRACC
+        titlesection: _LEADBLOCK* infocode+ NAMEBLOCK+
+        invtitlesection: NAMEBLOCK+ infocode+
+        endsection.3: (_SEPARATOR | notname) (_SEPARATOR | notname | MISCBLOCK)*
+        
+        infocode.3: icode
+        icode.3: "GMK"i -> gmk | "PBT"i -> pbt | ("ePBT"i | "EnjoyPBT"i) -> epbt | ("IFK"i | "Infinikey"i) -> ifk
+               | ("MG"i | "Melgeek"i) -> mg | "SA"i -> sa | ("SP"i | "SPSA"i | "Signature Plastics"i) -> sp
+               | "HSA"i -> hsa | "KAT"i -> kat | "KAM"i -> kam | "DSA"i -> dsa | "JTK"i -> jtk | "CRP"i -> crp
+               | "MDA"i -> mda | "XDA"i -> xda | "DCS"i -> dcs
+        gbstatus.2: /ship(ping|ed)*/i | /live/i | /clos(ed|ing)*/i | /complet(ed|e|ing)/i | /cancel(ed|led)/i
+               | /finish(ed|ing)*/i | /final(ized|izing)*/i | /sort(ed|ing)*/i
+               | /production/i | /extras*/i 
+               
+        notname.2: gbstatus | /key(cap|set)*s*/i | /GB|groupbuy|(group buy)/i | /ready/i | /\w+shot/i | /update[ds]*/i
+                
+        MISCBLOCK: /[\w-]+/
+        NAMEBLOCK: MISCBLOCK
+        _LEADBLOCK: MISCBLOCK
+        
+        _SEPARATOR: /[-:;,.\|~\\\/]+/
+        _BRACO: /[\[{(]/
+        _BRACC: /[]})]/
+        
+        
+        %import common.WS
+        %ignore WS
+        %ignore /[!'"]/
+    '''
 
-    else:
-        gbreturn = 'unknown'
+    # TODO: rewite grammar including whitespace (to better differentiate potential titles and separators)
+    # TODO: define some separators as needing WS on at least one side (-,.:;\/~) vs (|[](){}) which don't
+    # TODO: handle special characters in title/misc blocks
+    # TODO: handle unparseable case (i.e. not a keycap gb) -> by adding a 3rd option to topic rule
 
-    # TODO: add tai-hao (and potential variations like SPSA) to list of infocodes
-    type_set = ['GMK', 'PBT', 'ePBT', 'SA', 'HSA', 'KAT', 'KAM', 'DSA', 'IFK', 'JTK', 'CRP', 'SP', 'MDA', 'XDA', 'MG',
-                'INFINIKEY', 'MELGEEK', 'ENJOYPBT']
+    title_parser = Lark(title_grammar, start="topic", parser="lalr")
+    print(title_parser.parse(to_search).pretty())
 
-    sep_set = [r"[:;,.]\s", r"\s[-|~/\\[{(]", r"\sround", r"\sr\d"]
+    # TODO: refactor to have parse_title() be independent of clean_board_data()?
 
-    # find main/first info code (for keycap)
-    # TODO: add support for multiple infocodes
-    typereg = regex.compile(r"\b\L<type_set>\b", regex.IGNORECASE, type_set=type_set)
-    captype = typereg.search(to_search)
-    if captype:
-        typereturn = captype.group()
-    else:
-        typereturn = None
-    # types = typereg.finditer(to_search)
-    # typereturn = []
-    # for captype in types:
-    #     typereturn.append(captype.group())
+    # to_search = to_search.strip('" ')
+    #
+    # # find bracketed type indicator
+    # gbreg = regex.compile(r"[[{(](\w*)[]})]")
+    # gbtype = gbreg.match(to_search)
+    # if gbtype:
+    #     gbreturn = gbtype.group(1)
+    #
+    # else:
+    #     gbreturn = 'unknown'
+    #
+    # # TODO: add tai-hao (and potential variations like SPSA) to list of infocodes
+    # type_set = ['GMK', 'PBT', 'ePBT', 'SA', 'HSA', 'KAT', 'KAM', 'DSA', 'IFK', 'JTK', 'CRP', 'SP', 'MDA', 'XDA', 'MG',
+    #             'INFINIKEY', 'MELGEEK', 'ENJOYPBT']
+    #
+    # sep_set = [r"[:;,.]\s", r"\s[-|~/\\[{(]", r"\sround", r"\sr\d"]
+    #
+    # # find main/first info code (for keycap)
+    # # TODO: add support for multiple infocodes
+    # typereg = regex.compile(r"\b\L<type_set>\b", regex.IGNORECASE, type_set=type_set)
+    # captype = typereg.search(to_search)
+    # if captype:
+    #     typereturn = captype.group()
+    # else:
+    #     typereturn = None
+    # # types = typereg.finditer(to_search)
+    # # typereturn = []
+    # # for captype in types:
+    # #     typereturn.append(captype.group())
+    #
+    # # if we found a code, find likely keyset name in title
+    # if typereturn:
+    #     # tries to find likely separator between set name and status updates
+    #     # sepreg = regex.compile(r"\L<sep_set>", regex.IGNORECASE, sep_set=sep_set)
+    #     sepreg = regex.compile(r"\s[-|~/\\[{(]|[:;,.]\s", regex.IGNORECASE)
+    #     sep = sepreg.search(to_search, pos=5)
+    #
+    #     # get string between initial [] identifier and separator(if any), excluding info code
+    #     titlelimit = [0, len(to_search)]
+    #     if gbtype:
+    #         titlelimit[0] = gbtype.end()
+    #     if sep:
+    #         titlelimit[1] = sep.start()
+    #     setname = (to_search[titlelimit[0]:captype.start()] + to_search[captype.end():titlelimit[1]]).strip()
+    #
+    #     # if no separator, just take first word
+    #     if sep is None:
+    #         setname = setname.split()[0]
+    #
+    #     namereturn = setname
+    # else:
+    #     namereturn = None
+    #
+    # return gbreturn, typereturn, namereturn
 
-    # if we found a code, find likely keyset name in title
-    if typereturn:
-        # tries to find likely separator between set name and status updates
-        # sepreg = regex.compile(r"\L<sep_set>", regex.IGNORECASE, sep_set=sep_set)
-        sepreg = regex.compile(r"\s[-|~/\\[{(]|[:;,.]\s", regex.IGNORECASE)
-        sep = sepreg.search(to_search, pos=5)
-
-        # get string between initial [] identifier and separator(if any), excluding info code
-        titlelimit = [0, len(to_search)]
-        if gbtype:
-            titlelimit[0] = gbtype.end()
-        if sep:
-            titlelimit[1] = sep.start()
-        setname = (to_search[titlelimit[0]:captype.start()] + to_search[captype.end():titlelimit[1]]).strip()
-
-        # if no separator, just take first word
-        if sep is None:
-            setname = setname.split()[0]
-
-        namereturn = setname
-    else:
-        namereturn = None
-
-    return gbreturn, typereturn, namereturn
+    return
 
 
 def parse_titles(titles):
--- initialtest.py
+++ initialtest.py
@@ -1,13 +1,13 @@
 from ghscrape import *
 from forumclean import *
 
-scraped_data = scrape_board("https://geekhack.org/index.php?board=70.", limit_pages=3,
-                            request_interval=10, filepath='test.csv')
+# scraped_data = scrape_board("https://geekhack.org/index.php?board=70.", limit_pages=3,
+#                             request_interval=10, filepath='test.csv')
 # print(type(scraped_data))
 
-testdata = clean_board_data(scraped_data, filepath="data/test2.csv")
-
-print(testdata[['code', 'name']].head(45))
+# testdata = clean_board_data(scraped_data, filepath="data/test2.csv")
+#
+# print(testdata[['code', 'name']].head(45))
 
 # print(find_captype("[GB] TGR-Jane v2 CE (QC, Shipping) 50% completed "))
 # print(find_captype("[GB]Enjoypbt '紺桃KON MOMO' Japanese keycaps (7.10~7.30)"))
@@ -16,3 +16,5 @@ print(testdata[['code', 'name']].head(45))
 #                             request_interval=15, filepath="data/210108testraw.csv")
 
 # testdata = clean_board_data(scraped_data, filepath="data/210108test.csv")
+
+parse_title("[GB] test JTK / 'Vostok-1' // Восток-1 // Tripleshot")
