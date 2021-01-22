7b4fb2a316114afd7261eb1912988c3acab5d31b
Matt Salinero <matt.salinero@gmail.com>
2021-01-22 01:19:37 -0800

    feat: revise scraping script to simplify getting new data

--- scrapeforums.py
+++ scrapeforums.py
@@ -1,15 +1,26 @@
 from ghscrape import *
 from forumclean import *
-import csv
+import datetime
 
-# scraped_data = scrape_board("https://geekhack.org/index.php?board=70.", limit_pages=40,
-#                             request_interval=15, filepath="data/210109raw.csv")
-# scraped_data = scrape_board("https://geekhack.org/index.php?board=132.", limit_pages=65,
-#                             request_interval=15, filepath="data/210109ic_raw.csv")
 
-# rawdata = pd.read_csv("data/210109raw.csv")
-# clean_data = clean_board_data(rawdata, "data/210109gb_clean.csv")
-rawdata = pd.read_csv("data/210109ic_raw.csv")
-clean_data = clean_board_data(rawdata, "data/210109ic_clean.csv")
+date_code = datetime.datetime.now().strftime("%y%m%d")
+base_filepath = "data/" + date_code
+
+print("Scraping data for " + date_code)
+
+if input("scrape groupbuy forum? (Y/N): ").upper() == "Y":
+    raw_filepath = base_filepath + "gb_raw.csv"
+    clean_filepath = base_filepath + "gb_clean.csv"
+    scrape_board("https://geekhack.org/index.php?board=70.", limit_pages=40,
+                 request_interval=15, filepath=raw_filepath)
+    clean_board_data(infilepath=raw_filepath, outfilepath=clean_filepath)
+
+if input("scrape interest check forum? (Y/N): ").upper() == "Y":
+    raw_filepath = base_filepath + "ic_raw.csv"
+    clean_filepath = base_filepath + "ic_clean.csv"
+    scrape_board("https://geekhack.org/index.php?board=132.", limit_pages=65,
+                 request_interval=15, filepath=raw_filepath)
+    clean_board_data(infilepath=raw_filepath, outfilepath=clean_filepath)
+
 
 
