f63a7071413a78e38e1173b586abec38888b4013
Matt Salinero <matt.salinero@gmail.com>
2021-02-12 18:05:02 -0800

    feat: create and implement tests for append_csv(), prepare_appending_csv(), read_csv(), and write_post_json() within mech_io module

--- mech_io.py
+++ mech_io.py
@@ -1,16 +1,62 @@
 import sqlite3
 from pathlib import Path
+import os
 import csv
 import datetime
 import json
 
 
-def write_csv(data, filepath, fields, mode='w', overwrite=False):
+def write_csv(data, filepath, fields, overwrite=False):
     # TODO: implement a check if this file exists already, use overwrite param to control if it overwrites
-    with open(filepath, mode, encoding="utf-8", newline='') as out_csv:
+    if type(data) not in [list, dict]:
+        raise ValueError("input should be a list or a list of dicts to write")
+
+    with open(filepath, 'w', encoding="utf-8", newline='') as out_csv:
         out_writer = csv.DictWriter(out_csv, fieldnames=fields)
         out_writer.writeheader()
-        out_writer.writerows(data)
+        if type(data) == dict:
+            out_writer.writerow(data)
+        else:
+            out_writer.writerows(data)
+
+    return
+
+
+def append_csv(data, filepath, fields, overwrite=False):
+    # TODO: implement a check if this file exists already, use overwrite param to control if it overwrites
+    if type(data) not in [list, dict]:
+        raise ValueError("input should be a list or a list of dicts to write")
+
+    with open(filepath, 'a', encoding="utf-8", newline='') as out_csv:
+        out_writer = csv.DictWriter(out_csv, fieldnames=fields)
+        if type(data) == dict:
+            out_writer.writerow(data)
+        else:
+            out_writer.writerows(data)
+
+    return
+
+
+def prepare_appending_csv(filepath, fields):
+    if os.path.isfile(filepath):
+        with open(filepath, 'r', encoding="utf-8", newline='') as prep_csv:
+            prep_reader = csv.DictReader(prep_csv)
+            if prep_reader.fieldnames != fields:
+                raise ValueError("unexpected fields found in file at filepath")
+            return True
+    else:
+        with open(filepath, 'w', encoding="utf-8", newline='') as prep_csv:
+            prep_writer = csv.DictWriter(prep_csv, fieldnames=fields)
+            prep_writer.writeheader()
+        return True
+
+
+def read_csv(filepath):
+    with open(filepath, 'r', encoding="utf-8", newline='') as in_csv:
+        in_reader = csv.DictReader(in_csv)
+        read_data = [csv_row for csv_row in in_reader]
+
+    return read_data
 
 
 def gen_csvpath(suffix, folder=None, date=None):
@@ -22,3 +68,17 @@ def gen_csvpath(suffix, folder=None, date=None):
     filename = date + "_" + suffix + ".csv"
 
     return folder / filename
+
+
+def write_post_json(topic_id, data, folder=None):
+    if not folder:
+        folder = Path(__file__).parent / "data" / "post_data"
+
+    filename = "topic" + topic_id + "_postdata.json"
+    json_filepath = folder / filename
+
+    json_data = json.dumps(data, indent=4, default=(lambda x: x.__str__()))
+    with open(json_filepath, 'w') as json_file:
+        json_file.write(json_data)
+
+    return
--- tests/test_mech_io.py
+++ tests/test_mech_io.py
@@ -17,7 +17,7 @@ class TestGenCsvpath(TestCase):
 class TestWriteCsv(TestCase):
     def test_write_csv(self):
         test_data = [{'test_col': "test value"}]
-        test_filepath = gen_csvpath("test", Path(__file__).parent, "2000-01-01")
+        test_filepath = Path(__file__).parent / "fixtures" / "test_write.csv"
         write_csv(test_data, test_filepath, ['test_col'])
 
         with open(test_filepath, 'r', encoding="utf-8", newline='') as result_csv:
@@ -25,4 +25,113 @@ class TestWriteCsv(TestCase):
             result_data = [csv_topic for csv_topic in result_reader]
 
         self.assertEqual("test value", result_data[0]['test_col'])
+        os.remove(test_filepath)
+
+    def test_write_csv_single(self):
+        test_data = {'test_col': "test value"}
+        test_filepath = Path(__file__).parent / "fixtures" / "test_write.csv"
+        write_csv(test_data, test_filepath, ['test_col'])
+
+        with open(test_filepath, 'r', encoding="utf-8", newline='') as result_csv:
+            result_reader = csv.DictReader(result_csv)
+            result_data = [csv_topic for csv_topic in result_reader]
+
+        self.assertEqual("test value", result_data[0]['test_col'])
+        os.remove(test_filepath)
+
+    def test_write_csv_valueerror(self):
+        test_filepath = Path(__file__).parent / "fixtures" / "test_write.csv"
+
+        with self.assertRaises(ValueError):
+            write_csv(123, test_filepath, ['test_col'])
+            os.remove(test_filepath)
+
+
+class TestAppendCsv(TestCase):
+    def test_append_csv(self):
+        test_data = [{'test_col': "appended value"}]
+        test_filepath = Path(__file__).parent / "fixtures" / "test_append.csv"
+        with open(test_filepath, 'w', encoding="utf-8", newline='') as test_csv:
+            test_writer = csv.DictWriter(test_csv, fieldnames=['test_col'])
+            test_writer.writeheader()
+            test_writer.writerow({'test_col': "initial value"})
+
+        append_csv(test_data, test_filepath, ['test_col'])
+
+        with open(test_filepath, 'r', encoding="utf-8", newline='') as result_csv:
+            result_reader = csv.DictReader(result_csv)
+            result_data = [csv_topic for csv_topic in result_reader]
+
+        self.assertEqual("initial value", result_data[0]['test_col'])
+        self.assertEqual("appended value", result_data[1]['test_col'])
+        os.remove(test_filepath)
+
+    def test_append_csv_single(self):
+        test_data = {'test_col': "appended value"}
+        test_filepath = Path(__file__).parent / "fixtures" / "test_append.csv"
+        with open(test_filepath, 'w', encoding="utf-8", newline='') as test_csv:
+            test_writer = csv.DictWriter(test_csv, fieldnames=['test_col'])
+            test_writer.writeheader()
+            test_writer.writerow({'test_col': "initial value"})
+
+        append_csv(test_data, test_filepath, ['test_col'])
+
+        with open(test_filepath, 'r', encoding="utf-8", newline='') as result_csv:
+            result_reader = csv.DictReader(result_csv)
+            result_data = [csv_topic for csv_topic in result_reader]
+
+        self.assertEqual("initial value", result_data[0]['test_col'])
+        self.assertEqual("appended value", result_data[1]['test_col'])
+        os.remove(test_filepath)
+
+    def test_append_csv_valueerror(self):
+        test_filepath = Path(__file__).parent / "fixtures" / "test_append.csv"
+
+        with self.assertRaises(ValueError):
+            append_csv(123, test_filepath, ['test_col'])
+            os.remove(test_filepath)
+
+
+class TestPrepareCsv(TestCase):
+    def test_prepare_appending_csv_existing(self):
+        test_filepath = Path(__file__).parent / "fixtures" / "test_csv.csv"
+        self.assertTrue(prepare_appending_csv(test_filepath, ['test_col1', 'test_col2']))
+        with self.assertRaises(ValueError):
+            prepare_appending_csv(test_filepath, ['not_field', 'test_col2'])
+
+    def test_prepare_appending_csv_new(self):
+        test_filepath = Path(__file__).parent / "fixtures" / "test_prepare.csv"
+        test_fields = ['test_col1', 'test_col2']
+        prepare_appending_csv(test_filepath, test_fields)
+
+        with open(test_filepath, 'r', encoding="utf-8", newline='') as result_csv:
+            result_reader = csv.DictReader(result_csv)
+            result_fields = result_reader.fieldnames
+
+        self.assertEqual(test_fields, result_fields)
+        os.remove(test_filepath)
+
+
+class TestReadCsv(TestCase):
+    def test_read_csv(self):
+        test_filepath = Path(__file__).parent / "fixtures" / "test_csv.csv"
+        test_data = read_csv(test_filepath)
+
+        self.assertEqual(3, len(test_data))
+        self.assertEqual("col1_0", test_data[0]['test_col1'])
+        self.assertEqual("col2_1", test_data[1]['test_col2'])
+
+
+class TestWritePostJson(TestCase):
+    def test_write_post_json(self):
+        test_folder = Path(__file__).parent / "fixtures"
+        test_filepath = test_folder / "topic_test_postdata.json"
+        test_data = {"test_col1": "test_val", "test_col2": ["test_val1", "test_val2"]}
+
+        write_post_json("_test", test_data, test_folder)
+
+        with open(test_filepath, 'r', encoding="utf-8", newline='') as result_json:
+            result_data = json.load(result_json)
+
+        self.assertEqual(test_data, result_data)
         os.remove(test_filepath)
