e0d1c4d8cdc2bcdd4a5e50f019fc9afcb35c02f9
mattsalinero <75608506+mattsalinero@users.noreply.github.com>
2021-06-27 10:01:21 -0700

    feat: add interface for converting report

--- mech_convert.py
+++ mech_convert.py
@@ -4,6 +4,16 @@ from nbconvert.exporters import HTMLExporter
 from nbconvert.preprocessors import TagRemovePreprocessor
 from pathlib import Path
 
+def main():
+    full_nb = Path(__file__).parent / "mech_report.ipynb"
+    html_nb = Path(__file__).parent / "mech_report.html"
+
+    if input("generate html report from current jupyter notebook? (CONVERT/N): ").upper() == "CONVERT":
+        nb_export(full_nb, html_nb)
+
+    return
+
+
 def nb_export(notebook, out_path):
 
     # setup config w/ tags to remove extra code in output report
@@ -11,7 +21,7 @@ def nb_export(notebook, out_path):
 
     c.TagRemovePreprocessor.remove_cell_tags = ("hide_cell",)
     c.TagRemovePreprocessor.remove_all_outputs_tags = ('hide_output',)
-    c.TagRemovePreprocessor.remove_input_tags = ('hide_input',)
+    c.TagRemovePreprocessor.remove_input_tags = ('hide_inputs',)
     c.TagRemovePreprocessor.enabled = True
 
     # configure and run exporter
@@ -23,7 +33,11 @@ def nb_export(notebook, out_path):
     output = HTMLExporter(config=c).from_file(notebook)
 
     # write to output html file
-    with open(out_path,  "w") as out_file:
+    with open(out_path,  "w", encoding="utf-8") as out_file:
         out_file.write(output[0])
     
     return
+
+
+if __name__ == '__main__':
+    main()
